5e7002f7eec38c7daf00ff6d9287587734f59fec
park <rlqkrwls@naver.com>
2018-08-06 19:02:21 +0900

    사상자수 딥러닝 코드

--- Injured_count_model.R
+++ Injured_count_model.R
@@ -16,18 +16,27 @@ set.seed(4444)
 
 accident <- read.csv('Kor_Train_교통사망사고정보(12.1~17.6).csv')
 
-sample <- accident %>% filter(사상자수 != 1) # 사상자 수가 1인 경우를 제외한 데이터
-sample_one <- accident %>% filter(사상자수 == 1)
-sample_one <- sample_one[sample(1:nrow(sample_one),4500),]
+test <- accident[1:5000, ]
+train <- accident[5001:nrow(accident),]
+
+sample <- train %>% filter(사상자수 != 1) # 사상자 수가 1인 경우를 제외한 데이터
+sample_one <- train %>% filter(사상자수 == 1)
+sample_one <- sample_one[sample(1:nrow(sample_one),2500),]
 sample <- rbind(sample, sample_one)
 
 train.x <- data.matrix(sample %>% dplyr::select(c(4,13,14,16,17,18,19,20,22)))
 train.y <- sample$사상자수
-model <- mx.mlp(train.x, train.y, hidden_node=20, out_node=16, activation="relu", out_activation="softmax",
-                num.round=300, array.batch.size=50, learning.rate=0.01, momentum=0.6,
-                eval.metric=mx.metric.accuracy, dropout=0.01)
 
-preds = predict(model, train.x)
+test.x <- data.matrix(test %>% dplyr::select(c(4,13,14,16,17,18,19,20,22)))
+test.y <- test$사상자수
+
+model <- mx.mlp(train.x, train.y, hidden_node=15, out_node=20, activation="relu", out_activation="softmax",
+                num.round=1500, array.batch.size=50, learning.rate=0.01, momentum=0.9,
+                eval.metric=mx.metric.accuracy, eval.data = list(data = test.x, label = test.y))
+#결과 :
+#Train-accuacy = 0.61797980146376
+#Validation-accuacy = 0.712600001692772
+
+preds = predict(model, test.x)
 pred.label = max.col(t(preds))-1
-table(pred.label, train.y)
-
+table(pred.label, test.y)
