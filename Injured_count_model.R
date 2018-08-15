c2ee16f9c3e0a4593f982a3710986ebcf3999396
park <rlqkrwls@naver.com>
2018-08-15 23:07:36 +0900

    61% 짜리 모델 올립니다.

--- Injured_count_model.R
+++ Injured_count_model.R
@@ -22,20 +22,21 @@ sample_one <- train %>% filter(사상자수 == 1)
 sample_one <- sample_one[sample(1:nrow(sample_one),2500),]
 sample <- rbind(sample, sample_one)
 
-train.x <- data.matrix(sample %>% dplyr::select(c(4,13,14,16,17,18,19,20,22)))
+train.x <- data.matrix(sample %>% dplyr::select(c(4,6,14,16,17,18,19,20,22)))
 train.y <- sample$사상자수
 
-test.x <- data.matrix(test %>% dplyr::select(c(4,13,14,16,17,18,19,20,22)))
+test.x <- data.matrix(test %>% dplyr::select(c(4,6,14,16,17,18,19,20,22)))
 test.y <- test$사상자수
 
 set.seed(4444)
 mx.set.seed(4444)
-model <- mx.mlp(train.x, train.y, hidden_node=15, out_node=20, activation="relu", out_activation="softmax",
-                num.round=3000, array.batch.size=50, learning.rate=0.01, momentum=0.9,
+model <- mx.mlp(train.x, train.y, hidden_node=1000, out_node=500, activation="relu", out_activation="softmax",
+                num.round=1500, array.batch.size=50, learning.rate=0.001, momentum=0.9,
                 eval.metric=mx.metric.accuracy, eval.data = list(data = test.x, label = test.y))
 #결과 :
-#Train-accuacy = 0.61797980146376
-#Validation-accuacy = 0.712600001692772
+#Train-accuacy = 0.477707005799956
+#Validation-accuacy = 51200000077486
+
 
 preds = predict(model, test.x)
 pred.label = max.col(t(preds))-1
