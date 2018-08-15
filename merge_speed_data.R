b420cf483b00d149281a2220225cd11a558597a7
Junseo Lee <eonchan99@gmail.com>
2018-08-15 23:36:43 +0900

    파일명 변경 및 시군구 단위 취합

--- merge_speed_data.R
+++ merge_speed_data.R
@@ -103,16 +103,51 @@ for (i in c(1:24))
 }
 rm(tmp)
 
-# 연도 기준 통행속도
-ss<-speed %>% group_by(연도, 도로명, 시점명, 종점명, 링크ID, 거리, 방향) %>% summarise(X01시=mean(X01시),X02시=mean(X02시),X03시=mean(X03시),X04시=mean(X04시),X05시=mean(X05시),X06시=mean(X06시),
+# 통행속도
+ss<-speed %>% group_by(링크ID) %>% summarise(X01시=mean(X01시),X02시=mean(X02시),X03시=mean(X03시),X04시=mean(X04시),X05시=mean(X05시),X06시=mean(X06시),
                                                                   X07시=mean(X07시),X08시=mean(X08시),X09시=mean(X09시),X10시=mean(X10시),X11시=mean(X11시),X12시=mean(X12시),
                                                                   X13시=mean(X13시),X14시=mean(X14시),X15시=mean(X15시),X16시=mean(X16시),X17시=mean(X17시),X18시=mean(X18시),
                                                                   X19시=mean(X19시),X20시=mean(X20시),X21시=mean(X21시),X22시=mean(X22시),X23시=mean(X23시),X24시=mean(X24시))
 View(ss)
 
-ss_j <- left_join(ss, road, by=c("링크ID"))
+r <- road
+r$사고건수 = as.numeric(r$사고건수)
+r$사망자수 = as.numeric(r$사망자수)
+r$중상자수 = as.numeric(r$중상자수)
+r$경상자수 = as.numeric(r$경상자수)
+r$부상신고자수 = as.numeric(r$부상신고자수)
+
+ss_j <- left_join(ss, r, by=c("링크ID"))
+ss_j <- ss_j %>% filter(!is.na(시군구))
+ss_j$평균통행속도 <- (ss_j$X01시+ss_j$X02시+ss_j$X03시+ss_j$X04시+ss_j$X05시+ss_j$X06시+ss_j$X07시+ss_j$X08시+ss_j$X09시+ss_j$X10시+ss_j$X11시+ss_j$X12시+ss_j$X13시+ss_j$X14시+ss_j$X15시+ss_j$X16시+
+                    ss_j$X17시+ss_j$X18시+ss_j$X19시+ss_j$X20시+ss_j$X21시+ss_j$X22시+ss_j$X23시+ss_j$X24시)/24
 View(ss_j)
 
 View(head(traffic, 20))
 View(head(speed, 20))
 View(head(road, 20))
+
+# 시군구 별 통행속도 및 사고건수
+sigungu <- ss_j %>% group_by(시군구) %>% summarise(평균통행속도=mean(평균통행속도),사고건수=mean(사고건수), 사망자수=mean(사망자수), 중상자수=mean(중상자수), 경상자수=mean(경상자수), 부상신고자수=mean(부상신고자수))
+
+View(sigungu)
+
+sigungu$사상자수 <- sigungu$사망자수+sigungu$중상자수+sigungu$경상자수+sigungu$부상신고자수
+sigungu$통행속도대비사상자수 <- sigungu$사상자수/sigungu$평균통행속도
+
+
+
+# 원본 데이터에 붙여보겠음
+avg_speed <- mean(sigungu$평균통행속도)
+avg_cnt <- mean(sigungu$사고건수)
+avg_hurt <- mean(sigungu$사상자수)
+
+sigungu <- rename(sigungu, "발생지시군구"="시군구")
+sigungu <- rename(sigungu, "시군구사상자수"="사상자수")
+sigungu <- sigungu %>% select(발생지시군구, 평균통행속도, 사고건수, 시군구사상자수)
+
+accident <- left_join(accident, sigungu, by=c("발생지시군구"))
+# NA는 그냥 평균으로 하자
+accident$평균통행속도 <- ifelse(is.na(accident$평균통행속도), avg_speed, accident$평균통행속도)
+accident$사고건수 <- ifelse(is.na(accident$사고건수), avg_cnt, accident$사고건수)
+accident$시군구사상자수 <- ifelse(is.na(accident$시군구사상자수), avg_hurt, accident$시군구사상자수)
