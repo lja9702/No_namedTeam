3b1bba114947663016063df33b91ffdc42e43668
Junseo Lee <eonchan99@gmail.com>
2018-08-23 22:40:42 +0900

    수정

--- main.R
+++ main.R
@@ -66,6 +66,8 @@ test_data <- pre_input_doro_big(test_data, train_data)
 test_data <- preprocessing_based_accidentType(test_data)
 test_data <- preprocessing_based_dangsaja(test_data)
 
+test_clone <- test_data
+
 td <- train_data
 td$주야 <- as.ordered(td$주야)
 max(td$주야)
@@ -91,108 +93,57 @@ td$주야 <- as.ordered(td$주야)
 max(td$주야)
 td$주야 <- ifelse(is.na(td$주야), max(td$주야), td$주야)
 
+td$주야 <- as.ordered(td$주야)
+max(td$주야)
+td$주야 <- ifelse(is.na(td$주야), max(td$주야), td$주야)
+
+# 사망자수, 사상자수, 중상자수, 경상자수, 부상신고자수
+test_data$사망자수 <- ifelse(is.na(test_data$사망자수), mean(td$사망자수), test_data$사망자수)
+test_data$중상자수 <- ifelse(is.na(test_data$중상자수), mean(td$중상자수), test_data$중상자수)
+test_data$경상자수 <- ifelse(is.na(test_data$경상자수), mean(td$경상자수), test_data$경상자수)
+test_data$부상신고자수 <- ifelse(is.na(test_data$부상신고자수), mean(td$부상신고자수), test_data$부상신고자수)
 
   
 # TODO: input test_kor to models
 # predict_y(day_night_model, ~~~)
 c1 <- predict_y(day_night_model, day_night_x(test_data))
-c2 <- predict_y(week_model, test_data)
-c3 <- predict_y(injury_dead_cnt_model, test_data)
-c4 <- predict_y(injury_cnt_model, test_data)
-c5 <- predict_y(injury_mid_cnt_model, test_data)
-c6 <- predict_y(injury_weak_cnt_model, test_data)
-c7 <- predict_y(injury_call_cnt_model, test_data)
-c8 <- predict_y(sido_model, test_data)
-c9 <- predict_y(sigungu_model, test_data)
-c10 <- predict_y(accident_type_model, test_data) #dummy
-c11 <- predict_y(accident_type_model, test_data)
-c12 <- predict_y(violation_model, test_data)
-c13 <- predict_y(main_road_type_model, test_data)
-c14 <- predict_y(detail_road_type_model, test_data)
-c15 <- predict_y(attacker_model, test_data)
-c16 <- predict_y(victim_model, test_data)
+c2 <- predict_y(week_model, week_x(test_data))
+c3 <- predict_y(injury_dead_cnt_model, injury_dead_cnt_x(test_data))
+c4 <- predict_y(injury_cnt_model, injury_cnt_x(test_data))
+c5 <- predict_y(injury_mid_cnt_model, injury_mid_cnt_x(test_data))
+c6 <- predict_y(injury_weak_cnt_model, injury_weak_cnt_x(test_data))
+c7 <- predict_y(injury_call_cnt_model, injury_call_cnt_x(test_data))
+c8 <- predict_y(sido_model, sido_x(test_data))
+c9 <- predict_y(sigungu_model, sigungu_x(test_data))
+c10 <- predict_y(accident_type_model, accident_type_x(test_data)) #dummy
+c11 <- predict_y(accident_type_model, accident_type_x(test_data))
+c12 <- predict_y(violation_model, violation_x(test_data))
+c13 <- predict_y(main_road_type_model, main_road_type_x(test_data))
+c14 <- predict_y(detail_road_type_model, detail_road_type_x(test_data))
+c15 <- predict_y(attacker_model, attacker_x(test_data))
+c16 <- predict_y(victim_model, victim_x(test_data))
 
 predict_df <- cbind(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16)
 predict_df <- as.data.frame(predict_df)
 
-for(row in 1:nrow(test_data))
+for(row in 1:nrow(test_clone))
 {
-  for(col in 1:ncol(test_data))
+  for(col in 1:ncol(test_clone))
   {
-    if(is.na(test_data[row,col]))
+    if(is.na(test_clone[row,col]))
     {
-      if(col == 1)
-      {
-        test_data[row,col] <- predict_y(day_night_model, test_data)
-      }
-      else if(col == 2)
-      {
-        test_data[row,col] <- predict_y(week_model, test_data)
-      }
-      else if(col == 3)
-      {
-        test_data[row,col] <- predict_y(injury_dead_cnt_model, test_data)
-      }
-      else if(col == 4)
-      {
-        test_data[row,col] <- predict_y(injury_cnt_model, test_data)
-      }
-      else if(col == 5)
-      {
-        test_data[row,col] <- predict_y(injury_mid_cnt_model, test_data)
-      }
-      else if(col == 6)
-      {
-        test_data[row,col] <- predict_y(injury_weak_cnt_model, test_data)
-      }
-      else if(col == 7)
-      {
-        test_data[row,col] <- predict_y(injury_call_cnt_model, test_data)
-      }
-      else if(col == 8)
-      {
-        test_data[row,col] <- predict_y(sido_model, test_data)
-      }
-      else if(col == 9)
-      {
-        test_data[row,col] <- predict_y(sigungu_model, test_data)
-      }
-      else if(col == 10)
-      {
-        # 중분류 값을 참조해서 구하기
-      }
-      else if(col == 11)
-      {
-        test_data[row,col] <- predict_y(accident_type_model, test_data)
-      }
-      else if(col == 12)
-      {
-        test_data[row,col] <- predict_y(violation_model, test_data)
-      }
-      else if(col == 13)
-      {
-        test_data[row,col] <- predict_y(main_road_type_model, test_data)
-      }
-      else if(col == 14)
-      {
-        test_data[row,col] <- predict_y(detail_road_type_model, test_data)
-      }
-      else if(col == 15)
-      {
-        test_data[row,col] <- predict_y(attacker_model, test_data)
-      }
-      else if(col == 16)
-      {
-        test_data[row,col] <- predict_y(victim_model, test_data)
-      }
+      test_clone[row,col] <- predict_df[row, col]
     }
   }
 }
 
-pre_input_accidentType_big(test_data, train_data)
+test_clone$사고유형_대분류 <- NA
+test_clone$도로형태_대분류 <- NA
+pre_input_accidentType_big(test_clone, train_data)
+pre_input_doro_big(test_clone, train_data)
 
 # TODO: put result data
 
-read_res_and_input(result_data, test_data, "./")
+read_res_and_input(result_data, test_clone, "./")
 
 # TODO: save to result_kor
