adeed365aa9dd6fb58340e775d3b79d3bd62a562
Junseo Lee <eonchan99@gmail.com>
2018-08-23 20:18:18 +0900

    수정수정

--- setup_lib.R
+++ setup_lib.R
@@ -1,34 +1,39 @@
 #########################################################################################라이브러리설치
 
-cran <- getOption("repos")
-cran["dmlc"] <- "https://apache-mxnet.s3-accelerate.dualstack.amazonaws.com/R/CRAN/"
-options(repos = cran)
-options(repos = c(CRAN = "http://cran.rstudio.com"))
 if (!require(devtools)){
+  options(repos = c(CRAN = "http://cran.rstudio.com"))
   install.packages("devtools")
   require(devtools)
 }
 if (!require(mlbench)){
+  options(repos = c(CRAN = "http://cran.rstudio.com"))
   install.packages("mlbench")
   require(mlbench)
 }
 if (!require(mxnet)){
+  cran <- getOption("repos")
+  cran["dmlc"] <- "https://apache-mxnet.s3-accelerate.dualstack.amazonaws.com/R/CRAN/"
+  options(repos = cran)
   install.packages("mxnet")
   require(mxnet)
 }
 if (!require(readxl)){
+  options(repos = c(CRAN = "http://cran.rstudio.com"))
   install.packages("readxl")
   require(readxl)
 }
 if (!require(randomForest)){
+  options(repos = c(CRAN = "http://cran.rstudio.com"))
   install.packages("randomForest")
   require(randomForest)
 }
 if (!require(MASS)){
+  options(repos = c(CRAN = "http://cran.rstudio.com"))
   install.packages("MASS")
   require(MASS)
 }
 if (!require(dplyr)){
+  options(repos = c(CRAN = "http://cran.rstudio.com"))
   install.packages("dplyr")
   require(dplyr)
 }
